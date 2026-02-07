ed78dc4d71fdade8180dfd129bbc420291d16dcd
Adam Andersson <adam.andersson@elisapolystar.com>
2026-02-08 00:49:29 +0100

    depth resources

--- src/vulkan/vulkan_image.h
+++ src/vulkan/vulkan_image.h
@@ -2,11 +2,18 @@
 #define VULKAN_IMAGE_H
 
 #include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 #include "core.h"
 
 bool VulkanImage_CreateView(VkDevice device, VkImage image, VkFormat format,
                             VkImageAspectFlags aspect_flags, u32 mip_levels,
-                            VkImageView *image_view);
+                            VkImageView *image_view_out);
+
+bool VulkanImage_CreateDepthResources(VkDevice device, VkExtent2D image_extent,
+                                      VkPhysicalDeviceMemoryProperties device_memory_properties,
+                                      VkFormat depth_format, VkImage *image_out,
+                                      VkImageView *image_view_out,
+                                      VkDeviceMemory *device_memory_out);
 
 #endif
--- src/vulkan/vulkan_memory.h
+++ src/vulkan/vulkan_memory.h
@@ -2,6 +2,10 @@
 #ifndef VULKAN_MEMORY_H
 #define VULKAN_MEMORY_H
 
+
+typedef struct _vulkan_memory_manager_t vulkan_memory_manager_t;
+struct _vulkan_memory_manager_t;
+
 void VulkanMemory_Test();
 
 #endif
